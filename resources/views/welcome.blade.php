132efaa1f5b63818a5ee9bd2aea681f12a02b9df
Atomjoy <atomjoy.official@gmail.com>
2024-04-08 15:02:53 +0200

    Code highlightjs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,16 +13,16 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <!-- Highligt.js -->	
-	<script src="https://unpkg.com/@highlightjs/cdn-assets@11.7.0/highlight.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/highlightjs-line-numbers.js/dist/highlightjs-line-numbers.min.js"></script>
+    <!-- Highligt.js (only for code line numbers, delete if don't want line bumbers) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/highlightjs-line-numbers.js@2.8.0/dist/highlightjs-line-numbers.min.js"></script>
     <script type="text/javascript">		
         window.onload = (event) => {
             hljs.highlightAll();
 		    hljs.initLineNumbersOnLoad({ startFrom: 1 });
         }
     </script>
-    <!-- Highligt.js -->
+    <!-- Highligt.js (only for code line numbers) -->
 
     @vite('resources/css/app.css')
 </head>
